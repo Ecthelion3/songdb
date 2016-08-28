71db1296bd100a8e48db0d910bca96058ed3c43a
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-29 00:56:35 +0200

    add validation test for songs

---
+++ spec/models/song_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Song do
+  describe "validations" do
+    it "is invalid without a name" do
+      song = Song.new(name: "")
+      song.valid?
+      expect(song.errors).to have_key(:name)
+    end
+  end
+end
