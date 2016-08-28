7116fd9ab9dc9fb7b6a463a9af2cbf609fb42ae9
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-28 22:15:39 +0200

    add artist sorting

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,25 @@
 class ArtistsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @artists = Artist.all
+    @artists = Artist.order("#{sort_column} #{sort_direction}")
   end
 
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def sortable_columns
+    ["name", "created_at"]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "created_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
